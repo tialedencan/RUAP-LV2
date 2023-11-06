20260b08b482d3370e05dd173e9c6f33a8a4fb61
tialedencan <tialedencan@gmail.com>
2023-11-06 18:35:57 +0100

    Task 2 - Creating the Contact Manager API Controllers

---
+++ ContactManager/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        public string[] Get()
+        {
+            return new string[]
+            {
+                "Hello",
+                "World"
+            };
+        }
+    }
+}
